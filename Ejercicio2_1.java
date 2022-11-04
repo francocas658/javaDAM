646eea8e71a55dae58013ff7659f8e4524b6c2f1
francocas658 <117371700+francocas658@users.noreply.github.com>
2022-11-04 16:34:29 +0100

    Add files via upload

---
+++ Ejercicio2_1.java
@@ -0,0 +1,18 @@
+/**
+ * @author Franco Tomas Casco
+ */
+public class Ejercicio2_1 { 
+	public static void main (String[] args) {
+		int x=144, y=999;
+		double suma, resta, mult, div;
+		suma= x+y;
+		resta= x-y;
+		mult= x*y;
+		div= y/x;
+		System.out.println ("x= " +x+ " y=" +y);
+		System.out.println ("x+y= " +suma);
+		System.out.println ("x-y= " +resta);
+		System.out.println ("x*y= " +mult);
+		System.out.println ("y/x= " +div);
+	}
+}
